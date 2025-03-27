52674c01eec09a570c718a61eb877f5a10649da4
Mattia Giordano <giordano.mattia1996@gmail.com>
2025-03-27 21:52:13 +0100

    Creazione delle entities e dei DAO

--- src/main/java/com/example/demo/configuration/DaoContext.java
+++ src/main/java/com/example/demo/configuration/DaoContext.java
@@ -1,4 +1,4 @@
-package com.example.configuration;
+package com.example.demo.configuration;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
